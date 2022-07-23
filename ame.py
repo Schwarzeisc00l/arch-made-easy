e4efc9e6d427557af1e0c21b8cb1aeb584a5c727
Schwarzeisc00l <78359804+Schwarzeisc00l@users.noreply.github.com>
2022-07-23 23:23:25 +0300

    Add files via upload

---
+++ ame.py
@@ -0,0 +1,18 @@
+import os
+from os.path import exists
+print('Welcome to Arch Made Easy!')
+print('This script will help you learn Arch!')
+print('0-Apply easier .bashrc')
+print('1-Read the documentation')
+os.system('sudo pacman -Syu')
+choose = input('Please choose an option: ')
+if choose == 0:
+    file_exists = exists('/usr/bin/python3')
+    if file_exists == True:
+        os.system('python3 apply-bashrc.py')
+        print('Done! You can read the documentation now at [insert link]')
+    if file_exists == False:
+        os.system('sudo pacman -S python3')
+        os.system('python3 apply-bashrc.py')
+        print('Done! You can read the documentation now at [insert link]')
+
---
+++ apply-bashrc.py
@@ -0,0 +1,39 @@
+import os
+from os.path import exists
+str1 = 'echo "alias "'
+str2 = '="'
+str3 = '"'
+# alias update='yay -Syu'
+def echo(shortcut,command):
+    os.system(str1 + shortcut + '='  + "\'" + command + "\'" + '>> ~/.bashrc')
+
+
+
+echo('update','"yay -Syu"')
+echo('remove','"yay -Rsu"')
+echo('install','"yay -S"')
+echo('clear','"yay -Scc"')
+echo('admin','"sudo"')
+echo('switch-root','"su root"')
+echo('test-connection','"ping wikipedia.org"')
+echo('stop','"killall"')
+echo('kernel','"uname -r"')
+echo('edit','"nano"')
+echo('search-packet','"yay"')
+echo('clone','"git clone"')
+echo('shutdown','"shutdown now"')
+echo('copy','"cp"')
+echo('copy_folder','"cp -R"')
+echo('move','"mv"')
+echo('create-folder','"mkdir"')
+echo('create-file','"touch"')
+echo('delete','"rm -rf"')
+file_exists = exists('/usr/bin/neofetch')
+if file_exists == True:
+    echo('info','"neofetch"')
+if file_exists == False:
+    os.system('yay -S neofetch')
+    echo('info','"neofetch"')
+
+print('The process is complete!')
+print('You can exit now!')

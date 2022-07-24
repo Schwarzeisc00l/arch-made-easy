9c6e91ce4b4bef418c9f80ba42a383b57ba3e3a2
Schwarzeisc00l <78359804+Schwarzeisc00l@users.noreply.github.com>
2022-07-24 10:55:10 +0000

    Update ame.py

--- ame.py
+++ ame.py
@@ -9,9 +9,13 @@ if choose == 0:
     file_exists = exists('/usr/bin/python3')
     if file_exists == True:
         os.system('python3 apply-bashrc.py')
-        print('Done! You can read the documentation now at [insert link]')
+        print('Done! You can read the documentation now at https://github.com/Schwarzeisc00l/arch-made-easy/wiki')
     if file_exists == False:
         os.system('sudo pacman -S python3')
         os.system('python3 apply-bashrc.py')
         print('Done! You can read the Wiki now at https://github.com/Schwarzeisc00l/arch-made-easy')
+       
+  
+if choose == 1:
+    os.system('python -m webbrowser -t https://github.com/Schwarzeisc00l/arch-made-easy/wiki')
 
